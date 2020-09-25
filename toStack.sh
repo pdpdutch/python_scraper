3da0cdace2bc579d75e5a4b6b35902bfaf691423
Who is joe <me@anotherone.com>
2020-09-25 06:55:13 +0200

    backup

---
+++ toStack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd '/mnt/e/Users/Test/Desktop/remix os/python_scraper/scraped'
+stackfolder="/mnt/c/Users/Test/stack/compilations"
+allMp4=$(find . -iname "*.mp4" -print)
+echo "$allMp4"
+for mp4 in ${allMp4[@]}; do
+    from="$(pwd)${mp4:1}"
+    to="$stackfolder${mp4:1:15}${mp4:25}"
+    sudo mv "$from" "$to" -f
+done
+
+sudo rm -rf .
+
